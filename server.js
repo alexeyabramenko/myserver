afafc6dac3b5f8c7c534196c4b1421f7e353ed64
Alexey <aleksei.b91163@gmail.com>
2021-02-14 12:24:57 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     const arrUrl = [req.url];
     let body = null;
     arrUrl.forEach( url => {
-        if (url != 'favicon.ico'){
+        if (url != '/favicon.ico'){
             body = fs.readFileSync(`./online-store${url}`);
             res.end(body);
         }
