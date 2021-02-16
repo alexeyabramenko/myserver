873f6901a34fca6f8ff1a0341fee17e4972b1871
Alexey <aleksei.b91163@gmail.com>
2021-02-16 16:17:13 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const fs = require('fs');
+const mime = require('mime');
+const path = require('path');
 
 
 const server = http.createServer((req, res) => {
-    const body = fs.readFileSync(`./${req.url}`);
-    res.end(body);
+    let filePath = './' + req.url
+    res.setHeader('Content-Type', mime.getType(filePath));
+    fs.createReadStream(filePath).pipe(res);
 });
 
 const port = process.env.PORT || 3000;
