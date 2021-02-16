ad24514a27c6181d89cac7fd5cc89d8485aca888
Alexey <aleksei.b91163@gmail.com>
2021-02-16 10:38:24 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const fs = require('fs');
+
+
 const server = http.createServer((req, res) => {
-    const body = fs.readFileSync(`./online-store${req.url}`);
+    const body = fs.readFileSync(`./${req.url}`);
     res.end(body);
 });
 
@@ -9,5 +11,4 @@ const port = process.env.PORT || 3000;
 
 server.listen(port);
 
-console.log('Server started!');
-console.log(`${port}`);
+console.log(`Server started! Port ${port}`);
